3c30330b670c4ca7f78009daf8513032b8c8a3e6
snzigod <snzigod@hotmail.com>
2018-05-12 16:44:34 +0800

    miniO

---
+++ miniO-core/src/main/java/per/sn/mini/o/core/entity/Demo.java
@@ -0,0 +1,5 @@
+package per.sn.mini.o.core.entity;
+
+public class Demo {
+
+}
--- miniO-core/src/main/java/per/sn/mini/o/core/service/impl/DemoServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package per.sn.mini.o.core.service.impl;
-
-import per.sn.mini.o.api.service.DemoService;
-
-public class DemoServiceImpl implements DemoService {
-
-}
