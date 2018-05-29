010d83b322318527c98080dcf6443a84e4e035cb
snzigod <snzigod@hotmail.com>
2018-05-29 23:16:24 +0800

    miniO

--- miniO-core/src/main/java/per/sn/mini/o/core/service/DemoServiceImpl.java
+++ miniO-core/src/main/java/per/sn/mini/o/core/service/DemoServiceImpl.java
@@ -1,21 +1,26 @@
 package per.sn.mini.o.core.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import per.sn.mini.o.api.dto.DemoDto;
 import per.sn.mini.o.api.service.DemoService;
+import per.sn.mini.o.core.repository.DemoMapper;
 
 @Service
 public class DemoServiceImpl implements DemoService {
 
+	@Autowired
+	private DemoMapper demoMapper;
+
 	@Override
 	public String sayHello(String name) {
-		return "hello, " + name;
+		return "hello, " + demoMapper.selectByPrimaryKey("1").getName();
 	}
 
 	@Override
 	public String sayHello(DemoDto dto) {
-		return "hello, " + dto.getName();
+		return "hello, " + demoMapper.selectByPrimaryKey("1").getName();
 	}
 
 }
